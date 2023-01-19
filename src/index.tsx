66fc79d43f4f66aac4f0934f5dc59520ead67666
wzj <31604422@qq.com>
2023-01-19 20:48:17 +0800

    增加eslint配置

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
+const obj = {
+  name: 'kabuto',
+}
+
 root.render(<App />)
