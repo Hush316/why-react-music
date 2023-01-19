8bb47b5672ec973a44eb5d6d64418eb51389c33d
wzj <31604422@qq.com>
2023-01-19 20:17:53 +0800

    增加prettier配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
-import App from '@/App';
-import ReactDOM from 'react-dom/client';
+import App from '@/App'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-    <App />
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-
+root.render(<App />)
